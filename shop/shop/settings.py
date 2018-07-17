cc510a05cc2dc72017b8d3c3be35367a9b2c355a
johnny <mmmaaaxxx77@gmail.com>
2018-07-18 00:30:06 +0800

    no message

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1g))j_%nbgm_*&u=qc(p1^&7+fpo^=&1)#&*fh*p3h$_)i$y5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
