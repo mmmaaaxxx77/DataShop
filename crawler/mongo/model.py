a44c061ce75ccf7b4fce09a4c0dd1075fdae952e
johnny <mmmaaaxxx77@gmail.com>
2018-07-17 18:27:26 +0800

    no message

--- crawler/mongo/model.py
+++ crawler/mongo/model.py
@@ -7,12 +7,14 @@ connect('stock', host='datashop-mongo')
 class Stock(Document):
     stock_id = StringField(required=True)
     stock_name = StringField(required=True)
+    stock_type = StringField(required=True)
     create_date = DateTimeField(default=datetime.datetime.now)
 
 
 class ShareHolder(Document):
     stock_id = StringField(required=True)
     stock_name = StringField(required=True)
+    stock_type = StringField(required=True)
     position = StringField(required=True)
     name = StringField(required=True)
     stock_count = LongField(required=True)
