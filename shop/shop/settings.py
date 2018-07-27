6294d8b6d0c2449519fc241dc1576d402501347f
johnny <mmmaaaxxx77@gmail.com>
2018-07-28 02:20:02 +0800

    no message

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/tmp/static_root/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
